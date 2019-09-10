eaa5976f9e978dcfffbf229c0f71c43076a835fc
AltugAltintas <altuga@gmail.com>
2019-09-10 10:35:35 +0300

    question 09 is done

--- src/main/java/com/kodcu/question09/App.java
+++ src/main/java/com/kodcu/question09/App.java
@@ -25,9 +25,12 @@ public class App
     {
         Date start = new Date();
         Date end = new Date();
+
         Period p = new Period(start, end);
         System.out.println(p.end());
+
         end.setYear(78); // Modifies internals of p!
+
         System.out.println(p.end());
     }
 }
