8d173a5178648974add91fbcf82583e0e69461f2
AltugAltintas <altuga@gmail.com>
2020-08-13 18:04:18 +0300

    2nd solution

--- src/main/java/com/kodcu/question01/NutritionFacts.java
+++ src/main/java/com/kodcu/question01/NutritionFacts.java
@@ -16,20 +16,51 @@ TODO:
  */
 public class NutritionFacts {
 
-    private final int servingSize = 0; // must
-    private final int servings = 0; // must
-    private final int calories = 0; // optional
-    private final int fat = 0;  // optional
-    private final int sodium = 0; // optional
-    private final int carbohydrate = 0; // optional
-        
+    private int servingSize = 0; // must
+    private int servings = 0; // must
+    private int calories = 0; // optional
+    private int fat = 0;  // optional
+    private int sodium = 0; // optional
+    private int carbohydrate = 0; // optional
 
 
+    // Setters
+    public void setServingSize(int val) {
+        servingSize = val;
+    }
+
+    public void setServings(int val) {
+        servings = val;
+    }
+
+    public void setCalories(int val) {
+        calories = val;
+    }
+
+    public void setFat(int val) {
+        fat = val;
+    }
+
+    public void setSodium(int val) {
+        sodium = val;
+    }
+
+    public void setCarbohydrate(int val) {
+        carbohydrate = val;
+    }
 
 
     public static void main(String[] args) {
 
-        
+        NutritionFacts cocaCola = new NutritionFacts();
+        cocaCola.setServingSize(240);
+        cocaCola.setServings(8);
+        cocaCola.setCalories(100);
+        cocaCola.setSodium(35);
+        cocaCola.setCarbohydrate(27);
+
+        System.out.println(cocaCola);
     }
+
 
 }
