002cb99f311eca8ba692709322f053849977f503
AltugAltintas <altuga@gmail.com>
2020-08-15 19:23:56 +0300

    question 18 back to original

--- src/main/java/com/kodcu/question18/Stack.java
+++ src/main/java/com/kodcu/question18/Stack.java
@@ -10,48 +10,49 @@ TODO
  3 - What is the lesson  ?
 */
 
-public class Stack<E>  {
-
-    //private E[] elements;
-    private Object[] elements;
-
+public class Stack {
+    public Object[] elements;
     private int size = 0;
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
 
-     public Stack() {
-             elements = new Object[DEFAULT_INITIAL_CAPACITY];
-     }
 
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
 
-   public void push(E e) {
+    public void push(Object e) {
         ensureCapacity();
         elements[size++] = e;
     }
-    public E pop() {
-        if (size == 0)
+
+    public Object pop() {
+        if (size == 0) {
             throw new EmptyStackException();
-        @SuppressWarnings("unchecked") E result = (E) elements[--size];
-        elements[size] = null; // Eliminate obsolete reference
-        return result;
-    }
-    public boolean isEmpty() {
-        return size == 0;
-    }
-    private void ensureCapacity() {
-        if (elements.length == size)
-            elements = Arrays.copyOf(elements, 2 * size + 1);
-    }
-
-    public static void main(String[] args) {
-            Stack<String> stack = new Stack<>();
-            for (String arg : args)
-                stack.push(arg);
-            while (!stack.isEmpty()) {
-
-                String value = stack.pop();
-                System.out.println(value.toUpperCase());
-            }
         }
 
 
+        Object result = elements[--size];
+        elements[size] = null;
+        return result;
+    }
+
+    /**
+     * Ensure space for at least one more element, roughly doubling the capacity
+     * each time the array needs to grow.
+     */
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push("hello"); // write 12 and run again
+
+        String result = (String) stack.pop();
+        System.out.println(result.toUpperCase());
+
+    }
 }
